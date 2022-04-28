5e7dae81aa3ca8caf2af7db108063ab0e303737a
Jack Blakeston <jackblakeston93@gmail.com>
2022-04-28 18:24:27 +0200

    Change to typescript and remove comments

--- client/src/components/welcome/welcome.tsx
+++ client/src/components/welcome/welcome.tsx
@@ -1,7 +1,5 @@
-// Package imports
 import { useNavigate } from 'react-router-dom';
 
-// Local imports
 import { useUserContext } from '../contexts/user';
 import {
   footer,
@@ -15,15 +13,13 @@ import {
   privacyDisclosure
 } from '../helpers/welcome';
 
-// Styling
 import './welcome.scss';
 
 function Welcome () {
   const navigate = useNavigate();
-  // Contexts
+
   const [user, setUser] = useUserContext();
 
-  // User salary input's onValueChange
   function onValueChange (value) {
     setUser({
       ...user,
@@ -33,23 +29,18 @@ function Welcome () {
 
   return (
     <div className='welcome'>
-      {/* Header and logo */}
       {headerAndLogo}
       <main className='welcome-container'>
-        {/* Welcome message */}
         {welcomeMessage}
-        {/* Describing the app */}
         {introduction}
         {functionality}
         {privacyDisclosure}
-        {/* User details and theme preference */}
         {userForm({
           defaultValue: user.salary,
           onValueChange: onValueChange,
           buttonOnClick: () => navigate('/jobs')
         })}
       </main>
-      {/* APIs used */}
       {footer}
     </div>
   );
