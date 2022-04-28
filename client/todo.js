e7cc625be8ffcef8d911e3f765f4374bab94bea9
Fav8 <leonardo.damico7597@gmail.com>
2022-04-28 15:01:37 +0200

    added to do list

---
+++ client/todo.js
@@ -0,0 +1,19 @@
+//!CLIENT TODO
+//TODO Remove redundant comments
+//TODO Change imports to be more compact (E.g. app.jsx)
+//TODO Add router go to dashboard when clicking on logo
+//TODO look into Accessibility FocusStyleManager.onlyShowFocusOnTabs(); app.jsx line 27
+//TODO change contextUser name into something more appropriate
+//TODO implement browser location popup
+//TODO look into filteredJobs, is it redundant?
+//TODO simplify context structure, probably a couple of prop drilling would work fine without making the website really complex
+//TODO style the error page
+//TODO make everything feel a little bit more responsive (E.g.css hover on buttons)
+//TODO less text on welocme page (add a button to display full description)
+//TODO turn class strings into a module
+//TODO refactor userForm to make it more readable
+//TODO restructure components folder and change names
+//TODO improve styling popup window
+//? Strict mode incompatible with BlueprintJS lol look into that.
+
+//? Do you need use strict in every file?
---
+++ server/todo.js
@@ -0,0 +1,7 @@
+//!SERVER TODO
+//TODO Remove redundant comments
+//TODO refactor to have specific get request instead of filtering in the front end
+//TODO Remove ununsed table, models, and everything else
+//TODO Remove redundant id in job Schema
+//TODO Remove unused fields ?does this bring improvements, like speed or space complexity?
+//? Do you need use strict in every file?
