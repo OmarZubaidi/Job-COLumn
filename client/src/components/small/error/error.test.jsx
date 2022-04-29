5d9f0e04f99c29f195d5e870a42c3a8bab14a94e
Jack Blakeston <jackblakeston93@gmail.com>
2022-04-29 17:33:40 +0200

    Implement first test for Error

--- client/src/components/small/error/error.test.jsx
+++ client/src/components/small/error/error.test.jsx
@@ -1,5 +1,20 @@
-import React from 'react'
-import {rest} from 'msw'
-import {setupServer} from 'msw/node'
-import {render, fireEvent, waitFor, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react';
+import Error from './error';
+import { MemoryRouter } from 'react-router-dom';
+
+const RouterWrapper = ({ children }) => ( // TODO move into general file, we will need this in all components that include useNavigate
+  <MemoryRouter>
+    {children}
+  </MemoryRouter>
+);
+
+describe('Error component', () => {
+  test('should render text with correct text in the DOM', () => {
+    render(<Error />, { wrapper: RouterWrapper });
+    expect(screen.getByText('Error 404: Page not found')).toBeInTheDocument();
+  });
+
+  test('should render a <Back> component', () => {
+
+  });
+});
