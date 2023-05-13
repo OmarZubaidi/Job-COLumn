56c94afd60d14264bd76e2837f55ad28077c29c7
Omar Zubaidi <66521244+OmarZubaidi@users.noreply.github.com>
2023-05-13 13:58:47 +0100

    Combine contexts

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,7 @@ import { FocusStyleManager } from '@blueprintjs/core';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import {
-  FilteredJobsProvider,
-  FilterProvider,
-  JobsProvider,
-  SortProvider,
-  ThemeProvider,
-  UserProvider,
-} from './contexts';
+import { ContextsProvider } from './contexts';
 import { Error, Home, Jobs } from './pages';
 import './App.scss';
 
@@ -18,24 +11,14 @@ FocusStyleManager.onlyShowFocusOnTabs();
 
 export function App() {
   return (
-    <ThemeProvider>
-      <UserProvider>
-        <JobsProvider>
-          <FilterProvider>
-            <FilteredJobsProvider>
-              <SortProvider>
-                <Router basename={process.env.PUBLIC_URL}>
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/jobs" element={<Jobs />} />
-                    <Route path="/*" element={<Error />} />
-                  </Routes>
-                </Router>
-              </SortProvider>
-            </FilteredJobsProvider>
-          </FilterProvider>
-        </JobsProvider>
-      </UserProvider>
-    </ThemeProvider>
+    <ContextsProvider>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/*" element={<Error />} />
+        </Routes>
+      </Router>
+    </ContextsProvider>
   );
 }
